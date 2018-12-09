8ba161ed0483ac91e89e84fbc3bfb4b6e0f70fbf
rulamon <ruben.lamon@gmail.com>
2018-12-09 21:47:55 +0100

    added comments

--- 1.js
+++ 1.js
@@ -1,7 +1,9 @@
 function orbitalPeriod(arr) {
-  var GM = 398600.4418;
-  var earthRadius = 6367.4447;
+	var GM = 398600.4418;
+	var earthRadius = 6367.4447;
+  //formula for orbital period
   const formula = (index) => Math.round(2 * Math.PI * Math.sqrt(Math.pow((earthRadius + arr[index]["avgAlt"]), 3) / GM))
+  //use array.prototype.map() to return object with orbitalPeriod instead of avgAlt 
   return arr.map((el, index) => {
   	return {
   		name: arr[index]["name"], 
